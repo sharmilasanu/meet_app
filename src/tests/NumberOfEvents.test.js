b3150e59e43731146848d978e39d1c72ecedeef3
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-12-18 16:27:25 +0100

    feat : Integradtion test cases (App, Citysearch, Eventlist)

--- src/tests/NumberOfEvents.test.js
+++ src/tests/NumberOfEvents.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
+
 describe('<NumberOfEvents /> component', () => { 
     let NumberOfEventsWrapper;
     beforeAll(() => {
@@ -10,11 +11,15 @@ describe('<NumberOfEvents /> component', () => {
     test('render textbox', () => {
         expect(NumberOfEventsWrapper.find('.events_number')).toHaveLength(1);
     });
-    test('changing event in textbox', () => {
-        NumberOfEventsWrapper.setState({
-            query: '32'
-          });
-          expect(NumberOfEventsWrapper.state('no_of_events')).toBe(32);
-    });
+
+    test("change state when input changes", () => {
+     const eventObject = { target: { value: 32 } };
+     NumberOfEventsWrapper.find(".events_number").simulate(  "change",eventObject);
+     expect(NumberOfEventsWrapper.state("no_of_events")).toBe(32);
+     });
+    
+    test("show number of events input label", () => {
+        expect(NumberOfEventsWrapper.find(".numberOfEvents label")).toHaveLength(1);
+      });
 
 });
