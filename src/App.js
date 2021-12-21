20d79444e37bfa8afce7d901802e55d9ec57af7b
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-12-21 07:08:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,18 +11,39 @@ class App extends Component {
     events: [],
     locations: [],
     numberOfEvents: 32,
+    currentLocation: 'all',
+    errorText: '',
   }
-  updateEvents = (location, eventCount = this.state.eventCount) => {
-    getEvents().then((events) => {
-      const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
-      this.setState({
-        events: locationEvents,
-        numberOfEvents: eventCount,
-      });
-    });
-  }
+  updateEvents = async (location, numberOfEvents) => {
+		getEvents().then(events => {
+			const locationEvents =
+				location === 'all'
+					? events
+					: events.filter(event => event.location === location);
+			if (this.mounted) {
+				this.setState({
+					events: locationEvents.slice(0, this.state.numberOfEvents),
+					currentLocation: location,
+				});
+			}
+		});
+	};
+
+  updateNumberOfEvents = async e => {
+		const newNumber = e.target.value ? parseInt(e.target.value) : 32;
+		if (newNumber < 1 || newNumber > 32) {
+			return this.setState({
+				errorText: 'Please choose a number between 1 and 32.',
+				numberOfEvents: 0,
+			});
+		} else {
+			this.setState({
+				errorText: '',
+				numberOfEvents: newNumber,
+			});
+			this.updateEvents(this.state.currentLocation, this.state.numberOfEvents);
+		}
+	};
 
   componentDidMount() {
     this.mounted = true;
@@ -41,7 +62,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NumberOfEvents updateEvents={this.updateEvents}/>
+        <NumberOfEvents 
+        numberOfEvents={this.state.numberOfEvents}
+				updateNumberOfEvents={this.updateNumberOfEvents}
+        />
          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
         
