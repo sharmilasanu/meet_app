dfef923b43dc3336aa9fbb6c25b6e685dcd12963
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-12-23 04:44:31 +0100

    Feat  :  Error Alert for No of events  Input

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
         query: '',
         suggestions: [],
-        showSuggestions: undefined
+        showSuggestions: undefined,
+        infoText: ''
       }
       handleInputChanged = (event) => {
         const value = event.target.value;
         const suggestions = this.props.locations.filter((location) => {
           return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
-        this.setState({
-          query: value,
-          suggestions,
-        });
+        if (suggestions.length === 0) {
+          this.setState({
+            query: event.target.value,
+            infoText: 'We can not find the city you are looking for. Please try another city',
+          });
+        } else {
+          return this.setState({
+            query: event.target.value,
+            suggestions,
+            infoText:''
+          });
+        }
       };
       handleItemClicked = (suggestion) => {
         this.setState({
@@ -26,6 +36,8 @@ class CitySearch extends Component {
   render() {
     return (
         <div className="CitySearch">
+          <InfoAlert text={this.state.infoText} />
+          <label>Select the City </label>
             <input
         type="text"
         className="city"
